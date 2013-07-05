e1e890af810458fb756b336eab5871c6232a8db6
Dean Spirov <spirov92@gmail.com>
2013-07-05 20:04:25 +0300

    опции при стартиране: open <type> <file> или new <rule>

--- main.py
+++ main.py
@@ -3,17 +3,31 @@ import files
 import game
 import grid
 import ui
+import re
 
+g = None
 if len(sys.argv) > 1:
-    file = files.ListFile(sys.argv[1])
-    g = file.load()
+    if sys.argv[1] is 'open':
+        if sys.argv[2] in ['list', 'life1.06']:
+            file = files.ListFile(sys.argv[1])
+        elif sys.argv[2] in ['life', 'life1.05']:
+            file = files.LifeFile(sys.argv[1])
+        else:
+            print('Please specify file type!')
+        g = file.load()
+    elif sys.argv[1] == 'new':
+        match = re.match(r'^(H:)?B(\d+)[^\d\w]?S(\d+)$', sys.argv[2])
+        if match:
+            g = game.Game(grid.HexGrid() if match.group(1) else grid.Grid())
+            g.set_rules(map(int, match.group(2)), map(int, match.group(3)))
+        elif sys.argv[2] == 'hex':
+            g = game.Game(grid.HexGrid())
+            g.set_rules([2], [3, 4])
+        else:
+            g = game.Game(grid.Grid())
 else:
     g = game.Game(grid.Grid())
-    g.grid.live((2, 2))
-    g.grid.live((2, 4))
-    g.grid.live((3, 3))
-    g.grid.live((3, 4))
-    g.grid.live((4, 3))
 
-display = ui.GridDisplay(g)
-display.main()
+if g:
+    display = ui.GridDisplay(g)
+    display.main()
